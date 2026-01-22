7bc0a24d432f74a312110f0391f5ad977e431e9e
Szymon Graczyk <szymon@graczyk.dev>
2026-01-22 22:49:59 +0100

    refactor: extract site description to consts

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_DESCRIPTION =
+  'Bitcraft Apps — web experiences and tools for Bitcraft and beyond.';
